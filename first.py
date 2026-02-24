0d2085ebd3a02f335bfaaf5bfafe3f2a79c272d1
Dinesh-2026 <dineshsanthosh6@gmail.com>
2026-02-25 03:12:46 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,3 +1,50 @@
-for i in range(10):
-    print(i)  
+// function to check whether mid speed is enough
+// to eat all piles of bananas under k hours
+function check(arr, mid, k) {
+    let totalHours = 0;
+    for (let i = 0; i < arr.length; i++) {
+        totalHours += Math.floor((arr[i] + mid - 1) / mid);
+    }
+
+    // return true if required time is less than 
+    // or equals to given hour, otherwise return false
+    return totalHours <= k;
+}
+
+function kokoEat(arr, k) {
+    // minimum speed of eating is 1 banana/hours
+    let lo = 1;
+
+    // maximum speed of eating is 
+    // the maximum bananas in given piles
+    let hi = Math.max(...arr);
+    let res = hi;
+
+    while (lo <= hi) {
+        let mid = lo + Math.floor((hi - lo) / 2);
+
+        // check if the mid(hours) is valid
+        if (check(arr, mid, k) === true) {
+          
+            // if valid continue to search at
+            // lower speed
+            hi = mid - 1;
+            res = mid;
+        }
+        else {
+          
+            // if cant finish bananas in given
+            // hours, then increase the speed
+            lo = mid + 1;
+        }
+    }
+    return res;
+}
+
+// Driver Code
+let arr = [5, 10, 3];
+let k = 4;
+console.log(kokoEat(arr, k));
+
+
           
